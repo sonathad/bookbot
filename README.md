11791ec17650be100c372463f77461e870b9dbfb
Thanos Dimitriades <sonathad@proton.me>
2024-06-01 22:36:37 +0300

    feat: get path from command line

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-BookBot is my first project!
+# BookBot
+
+A helper Python script that reads the contents of a file
+and prints the word count and letter count, ordered by frequency.
+
+## Usage
+
+Python 3 is required!
+
+Open folder in console and execute the following:
+
+`python main.py path/to/file`
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-def main():
-    path = "books/frankenstein.txt"
-    contents = get_contents(path)
+import sys
+
+def main(filepath):
+    contents = get_contents(filepath)
     if contents is not None:
         words_counter = word_count(contents)
         chars_counter = char_count(contents)
-        print_report(path, words_counter, chars_counter)
+        print_report(filepath, words_counter, chars_counter)
 
-def print_report(path, words_counter, chars_counter):
-    print(f"--- Begin report of {path} ---")
+def print_report(filepath, words_counter, chars_counter):
+    print(f"\n--- Begin report of {filepath} ---")
     print(f"{words_counter} words found in the document\n")
 
     # printing letter counts, ordered by frequency
@@ -15,7 +16,7 @@ def print_report(path, words_counter, chars_counter):
     for char in sorted_chars:
         print(f"The '{char}' character was found {chars_counter[char]} times")
     
-    print("--- End report ---")
+    print("\n--- End report ---")
 
 
 
@@ -44,4 +45,8 @@ def char_count(text):
     return chars
 
 if __name__ == "__main__":
-    main()
+    args = sys.argv
+    if len(args) == 2:
+        main(args[1])
+    else:
+        print("Usage: python main.py path/to/textfile")
